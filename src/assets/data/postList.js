17728a6d66dfdd17fd19eb8ee690d41f14182258
karolina0372 <karolinaignatuk@gmail.com>
2026-01-08 16:01:17 +0200

    fix: images paths for prod build

--- src/assets/data/postList.js
+++ src/assets/data/postList.js
@@ -1,56 +1,65 @@
+import img1 from '@/assets/images/1.png'
+import img2 from '@/assets/images/2.png'
+import img3 from '@/assets/images/3.png'
+import img4 from '@/assets/images/4.png'
+import img5 from '@/assets/images/5.png'
+import img6 from '@/assets/images/6.png'
+import img7 from '@/assets/images/7.png'
+import img8 from '@/assets/images/8.png'
+
 export const postList = [
   {
     id: 1,
-    image: 'src/assets/images/1.png',
+    image: img1,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
   },
   {
     id: 2,
-    image: 'src/assets/images/2.png',
+    image: img2,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
   },
   {
     id: 3,
-    image: 'src/assets/images/3.png',
+    image: img3,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
   },
   {
     id: 4,
-    image: 'src/assets/images/4.png',
+    image: img4,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
   },
   {
     id: 5,
-    image: 'src/assets/images/5.png',
+    image: img5,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
   },
   {
     id: 6,
-    image: 'src/assets/images/6.png',
+    image: img6,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
   },
   {
     id: 7,
-    image: 'src/assets/images/7.png',
+    image: img7,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
   },
   {
     id: 8,
-    image: 'src/assets/images/8.png',
+    image: img8,
     date: '2024-12-05',
     likes: 128,
     comments: 12,
